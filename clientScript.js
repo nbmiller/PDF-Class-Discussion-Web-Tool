c5acc1ed4c30ff8e6822f46585f826a7c00c7dae
nbmiller <nbmiller@sfu.ca>
2020-04-09 20:33:16 -0700

    FIRST UPLOAD

---
+++ clientScript.js
@@ -0,0 +1,451 @@
+
+// var URL = "http://localhost:8080"
+// var URL = 'http://24.207.30.121:3000'
+// var URL = 'http://host-24-207-30-121.public.eastlink.ca:3000'
+var URL = 'https://class-pdf-discussion-tool.herokuapp.com/'
+
+/*********************************************************
+**  GOOGLE SIGN IN
+**********************************************************/
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  // Do not send to backend! Use an ID token
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+   // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token;
+  var name = profile.getName()
+  var imgurl = profile.getImageUrl()
+  var email = profile.getEmail()
+  // var usertype = document.getElementById('userType').value
+//send sign in post to server
+
+// gapi.auth2.getAuthInstance().disconnect().then(function () {
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+
+  $.ajax({
+        type: 'POST',
+        url: URL + '/signin',
+        data: { id_token: id_token, name: name, imgurl: imgurl, email:email },
+        dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+          console.log("Sign in Success!")
+          console.log(data)
+          //redirect page to given URL
+          window.location.assign(data.url);
+        }
+      });
+    // }
+}
+
+/*********************************************************
+** SEND USER TYPE
+**********************************************************/
+function sendUserType() {
+
+  var user = getCookie('username')
+  var imgurl = getCookie('imgurl')
+  var email = getCookie('email')
+  var usertype = document.getElementById('userType').value
+//send sign in post to server
+
+
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+  $.ajax({
+        type: 'POST',
+        url: URL + '/userType',
+        data: { name: user, imgurl: imgurl, email:email, userType: usertype },
+        dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+          console.log("Sign in Success!")
+          console.log(data)
+          //redirect page to given URL
+          window.location.assign(data.url);
+        }
+      });
+
+}
+
+function studentStuff(){
+  getUser();
+  getClasses();
+}
+
+/*********************************************************
+** GET CLASSES
+**********************************************************/
+function getClasses() {
+
+  // var user = getCookie('username')
+  // var imgurl = getCookie('imgurl')
+  // var email = getCookie('email')
+  // var usertype = document.getElementById('userType').value
+//send sign in post to server
+
+
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+  $.ajax({
+        type: 'GET',
+        url: URL + '/getClasses',
+        // data: { name: user, imgurl: imgurl, email:email, userType: usertype },
+        // dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+
+          console.log("Classes:")
+          console.log(data)
+          var select = document.getElementById("class");
+
+          data.forEach(function(c) {
+            var option = document.createElement("option");
+            option.text = c;
+            option.value = c;
+            select.appendChild(option);
+          });
+
+        }
+      });
+
+}
+
+/*********************************************************
+** GET LESSONS
+**********************************************************/
+function getLessons() {
+
+  // var user = getCookie('username')
+  // var imgurl = getCookie('imgurl')
+  // var email = getCookie('email')
+  var className = document.getElementById('class').value
+  console.log('class name selected is ', className)
+//send sign in post to server
+  var middle = document.getElementById('middle')
+
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+  $.ajax({
+        type: 'POST',
+        url: URL + '/getLessons',
+        data: { className: className },
+        dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+
+          console.log("Lessons:")
+          console.log(data)
+
+          var select = document.createElement("select");
+          select.id = 'lesson'
+
+          data.forEach(function(c) {
+            var option = document.createElement("option");
+            option.text = c;
+            option.value = c;
+            select.appendChild(option);
+          });
+          middle.innerHTML = 'Lesson Names For: ' + className + '<br>';
+          middle.appendChild(select);
+          var button = document.createElement("button");
+
+          // button.setAttribute("value", 'Submit')
+          button.innerHTML = 'Submit'
+
+          middle.appendChild(button);
+          // button.setAttribute("onclick", fetchLesson)
+          button.onclick = fetchLesson
+
+        }
+      });
+
+}
+
+/*********************************************************
+**FETCH LESSON
+**********************************************************/
+function fetchLesson() {
+
+  var classN = getCookie('class')
+  console.log('class cookie: ', classN)
+  // var imgurl = getCookie('imgurl')
+  // var email = getCookie('email')
+  // var classN = document.getElementById('classN').value
+  var lesson = document.getElementById('lesson').value
+  // var url = document.getElementById('url').value
+
+  // var url = formatPDF(url)
+//send sign in post to server
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+  $.ajax({
+        type: 'POST',
+        url: URL + '/fetchLesson',
+        data: { classN: classN, lesson: lesson},
+        dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+          console.log("Lesson Fetched!")
+          console.log(data)
+          //redirect page to given URL
+          window.location.assign(data.url);
+        }
+      });
+
+}
+/*********************************************************
+**CREATE LESSON
+**********************************************************/
+function createLesson() {
+
+  var user = getCookie('username')
+  var imgurl = getCookie('imgurl')
+  var email = getCookie('email')
+  var classN = document.getElementById('classN').value
+  var lesson = document.getElementById('lessonN').value
+  var url = document.getElementById('url').value
+
+  var url = formatPDF(url)
+//send sign in post to server
+
+      //Do stuff here after the user has been signed out, you can still authenticate the token with Google on the server side
+
+  $.ajax({
+        type: 'POST',
+        url: URL + '/createLesson',
+        data: { name: user, imgurl: imgurl, email:email, classN: classN, lesson: lesson, url: url},
+        dataType: 'json',
+        async: true,
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+           alert("some error");
+        },
+        success: function (data) {
+          console.log("Sign in Success!")
+          console.log(data)
+          //redirect page to given URL
+          window.location.assign(data.url);
+        }
+      });
+
+}
+
+/*********************************************************
+** DISPLAY USERNAME AND PROFILE PIC
+**********************************************************/
+function getUser(){
+  var user = getCookie('username')
+  var imgu = getCookie('imgurl')
+  var stock = getCookie('stock')
+  var middletop = document.getElementById('middletop')
+
+  middletop.innerHTML = 'Welcome ' + user + '!'
+
+  var img = document.createElement("img");
+  img.src = imgu
+  img.width = 25
+  img.height = 25
+  img.style.paddingLeft = '5px'
+  img.className= 'help'
+  middletop.appendChild(img);
+}
+
+/*********************************************************
+** RETRIEVE COOKIE OF NAME GIVEN IN PARAMS
+**********************************************************/
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+/*********************************************************
+** FORMAT PDF
+**********************************************************/
+
+function formatPDF() {
+  var input = document.getElementById('url').value
+if(input.includes('view?usp=sharing') == true){
+  input = input.replace('view?usp=sharing','preview')
+}
+else{
+  input = input + '/preview'
+}
+if(input.includes('open?id=') == true){
+  input = input.replace('open?id=','file/d/')
+}
+console.log(input)
+
+return input
+
+  }
+
+
+
+/*********************************************************
+** PREVIEW PDF
+**********************************************************/
+
+function showPDF() {
+  var input = getCookie('url')
+
+              $('#embdLink').attr('src', input)
+
+  }
+
+
+
+/*********************************************************
+** RUN USER AND COMPANY INFO FUNCTIONS
+**********************************************************/
+function completeActions(){
+  getUser();
+  showPDF();
+  joinRoom();
+  // getCompany();
+}
+
+
+// var socket = io.connect(window.location.protocol + '//' + window.location.hostname);
+var socket = io(URL);
+// var socket = io();
+
+// var socket = io(URL, {transports: ['websocket']});
+// let socket = io.connect(window.location.href);
+// var socket = io.connect(URL);
+// var socket = io.connect(URL, {
+//       transports: ['polling']
+//    });
+
+// io.origins((origin, callback) => {
+//      if (origin !== URL) {
+//          return callback('origin not allowed', false);
+//      }
+//     callback(null, true);
+// });
+
+
+function joinRoom(){
+  var lesson = getCookie('lessonID')
+  socket.emit('join', {lessonID: lesson} ) ;
+}
+
+/*
+** On Socket Message Sent
+**/
+socket.on('getChat', function(data){
+  console.log(data.chat)
+  if (data.chat != undefined || data.chat != 0){
+    data.chat.forEach((item, i) => {
+      var isScrolledToBottom = output.scrollHeight - output.clientHeight <= output.scrollTop + 1;
+        output.innerHTML +=  item
+        //empty message text field
+        var message = document.getElementById('message').value ="";
+
+        // allow 1px inaccuracy by adding 1
+
+        if(isScrolledToBottom)
+            output.scrollTop = output.scrollHeight - output.clientHeight;
+    });
+  }
+
+
+
+});
+
+/*
+** Send Message To Other Player
+******************************************************************************
+**/
+
+var input = document.getElementById("message");
+// if(input != null){
+  // Execute a function when the user releases a key on the keyboard
+  input.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key
+    if (event.keyCode === 13) {
+      // Trigger the button element with a click
+      document.getElementById("send").click();
+    }
+  });
+// }
+
+function sendMsg(){
+  var classN = getCookie('classN')
+  var lesson = getCookie('lesson')
+    var usern =  getCookie('username')
+    var lessonID = getCookie('lessonID')
+    var message = document.getElementById('message').value;
+    var anon = document.getElementById('anon');
+    // var output = document.getElementById('output');
+    if(message !==""){
+      if(anon.checked == true){
+        socket.emit('sendMessage', {classN: classN,userName: 'Anonymous', lessonID: lessonID,
+          message: message});
+          // anon = ''
+      }
+      else{
+        socket.emit('sendMessage', {classN: classN,userName: usern, lessonID: lessonID,
+          message: message});
+      }
+      // var gameID = getGameID();
+
+    }
+}
+
+
+
+/*
+** On Socket Message Sent
+**/
+socket.on('messageSent', function(data){
+
+
+  var isScrolledToBottom = output.scrollHeight - output.clientHeight <= output.scrollTop + 1;
+    output.innerHTML += '<p><strong>' + data.userName +
+    ":  </strong>"+ data.message +'</p>'
+    //empty message text field
+    var message = document.getElementById('message').value ="";
+
+    // allow 1px inaccuracy by adding 1
+
+    if(isScrolledToBottom)
+        output.scrollTop = output.scrollHeight - output.clientHeight;
+});
---
+++ server.js
@@ -0,0 +1,418 @@
+
+var express = require('express');
+var app = express();
+var serverIndex = require('serve-index');
+var http = require('http');
+var bodyParser = require("body-parser");
+// var socketIO = require('socket.io');
+// var io = require('socket.io')(http);
+var path = require('path');
+// var server = http.createServer(app).listen(8080);
+var server = http.createServer(app).listen(3000);
+
+var fs = require('fs');
+var io = require('socket.io')(server);
+var cookieParser = require('cookie-parser');
+const {spawn} = require('child_process');
+let {PythonShell} = require('python-shell')
+
+var mongoose = require('mongoose');
+var mongodb = require('mongodb');
+var generator = require('mongoose-gen');
+const {OAuth2Client} = require('google-auth-library');
+var io = require('socket.io')(server);
+// const expressStatusMonitor = require('express-status-monitor');
+// app.use(expressStatusMonitor({
+//   websocket: io,
+//   port: app.get('port')
+// }));
+
+
+
+
+mongoose.connect('mongodb://root:root12@ds050559.mlab.com:50559/240project',{  useNewUrlParser: true });
+
+//Google Client ID
+var CLIENT_ID = '1010765927944-ecgak7pup964bph4q3ej3kn018455et7.apps.googleusercontent.com'
+
+
+var db = mongoose.connection;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+/*********************************************************
+** SCHEMA
+** STUFF
+*********************************************************/
+var Schema = mongoose.Schema;
+// instantiate the constructor
+
+/*
+** User Schema
+*/
+var userSchema= new Schema({
+  username:{type: String},
+  id_token:{type: String}, //only save ID token, full ID is unsafe
+  imgurl:{type: String},
+  email:{type: String, unique: true},
+  userType: {type: String}
+});
+
+var classSchema= new Schema({
+  name:{type: String, unique: true},
+  // lessons: [{
+  //   lessonName: {type: String}
+  //   // lessonName: {type: String,ref: 'Lesson'}
+  // }]
+  lessons: {
+    type: Array,
+          default: []
+    // lessonName: {type: String,ref: 'Lesson'}
+  }
+
+});
+
+var lessonSchema= new Schema({
+  name:{type: String},
+  // className:{type: String},
+  className:{type: String , ref: 'Class'},
+  url: {type: String},
+  chat: {
+    type: Array,
+          default: []
+    // lessonName: {type: String,ref: 'Lesson'}
+  }
+});
+
+
+// create a new model
+var User = mongoose.model('User', userSchema);
+var Class = mongoose.model('Class', classSchema);
+var Lesson = mongoose.model('Lesson', lessonSchema);
+/*
+** On Database Connection
+*/
+db.once('open', function(){
+console.log('DB Connection Success');
+});
+
+
+/***********************************************************
+** POSTS & GETS
+** STUFF
+***********************************************************/
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm','html'],
+  index: "index.html"
+}
+
+app.use('/', function(req,res,next){
+  console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
+  next();
+});
+
+app.use('/', express.static('./', options));
+
+
+
+/*******************************************************
+** User Sign In
+********************************************************/
+app.post('/signin', function(req,res,next){
+  var userName = req.body.name;
+  console.log("Trying to sign in ",userName);
+  //get user log in information
+  var id_token = req.body.id_token;
+  var email = req.body.email;
+  var imgurl = req.body.imgurl;
+  // var usertype = req.body.userType;
+
+//Verify login Information with Google Again
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: id_token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+}
+verify().catch(console.error);
+
+//Search Database if user exists, if not add user to DB
+  var query = { 'email':email};
+
+  User.findOneAndUpdate(query, { username: userName, imgurl: imgurl, id_token: id_token },
+         {upsert:true, new: true}, function(err, doc){
+        if (err)
+          console.log(err);
+        else{
+          console.log(doc.username, " successfully logged in!");
+          //save user information to cookies in order to use later
+          res.cookie('username' , doc.username);
+          res.cookie('imgurl' , doc.imgurl);
+          res.cookie('email' , doc.email);
+          // res.cookie('usertype' , doc.userType);
+          //send user to next page
+          res.send({url:'./unknownLogIn.html'})
+
+          // if(usertype == 'teacher'){
+          //   res.send({url:'./loggedin.html'})
+          // }
+          // else if(usertype == 'student'){
+          //   res.send({url:'./studentLoggedin.html'})
+          // }
+        }
+      });
+  });
+
+  /*******************************************************
+  ** User Type
+  ********************************************************/
+  app.post('/userType', function(req,res,next){
+    var userName = req.body.name;
+    console.log("Trying to set user type of ",userName);
+    //get user log in information
+    var id_token = req.body.id_token;
+    var email = req.body.email;
+    var imgurl = req.body.imgurl;
+    var usertype = req.body.userType;
+
+  //Search Database if user exists, if not add user to DB
+    var query = { 'email':email};
+
+    User.findOneAndUpdate(query, { username: userName, imgurl: imgurl, id_token: id_token, userType: usertype },
+           {upsert:true, new: true}, function(err, doc){
+          if (err)
+            console.log(err);
+          else{
+            console.log(doc.username, " updated type!");
+            res.cookie('usertype' , doc.userType);
+            if(usertype == 'teacher'){
+              res.send({url:'./loggedin.html'})
+            }
+            else if(usertype == 'student'){
+              res.send({url:'./studentLoggedin.html'})
+            }
+          }
+        });
+    });
+
+/*******************************************************
+** CREATE LESSON
+********************************************************/
+app.post('/createLesson', function(req,res,next){
+  var userName = req.body.name;
+  console.log("Creating Lesson from ",userName);
+  //get user log in information
+  var id_token = req.body.id_token;
+  var email = req.body.email;
+  var imgurl = req.body.imgurl;
+  // var usertype = req.body.userType;
+  var classN = req.body.classN;
+  var lesson = req.body.lesson;
+  var url = req.body.url;
+
+
+  var query = { 'name':classN};
+
+  Class.findOneAndUpdate(query, { name: classN, $push: {lessons: lesson} },
+    // $push: {lessons: [{lessonName: lesson}]}
+         {upsert:true, new: true}, function(err, doc){
+        if (err)
+          console.log(err);
+        else{
+          console.log(doc.name, " Class Created/Updated!");
+          res.cookie('classN' , doc.name);
+          res.cookie('lesson' , lesson);
+          res.cookie('url' , url);
+
+          var query = { 'name': lesson, 'className': classN};
+          Lesson.findOneAndUpdate(query, { name: lesson, className: classN, url: url },
+                 {upsert:true, new: true}, function(err, doc){
+                if (err)
+                  console.log(err);
+                else{
+                  console.log(doc.name, " Lesson Created/Updated!");
+
+                  res.cookie('lessonID', String(doc._id))
+                    res.send({url:'./lesson.html'})
+
+                }
+              });
+
+        }
+      });
+  });
+
+  /*******************************************************
+  ** Get Classes
+  ********************************************************/
+  app.get('/getClasses', function(req,res,next){
+
+    console.log("Trying to fetch Classes ");
+    Class.find({}, function(err, classes) {
+        // var classMap = {};
+        var classMap = [];
+
+        classes.forEach(function(c) {
+          classMap.push(c.name)
+
+        });
+
+        res.send(classMap);
+    });
+    //save stock user selected as a cookie
+    // res.cookie('stock' , stock);
+    //send to next page
+    // res.send({url:'./getstock.html'})
+    });
+
+
+    /*******************************************************
+    ** Get Lessons
+    ********************************************************/
+    app.post('/getLessons', function(req,res,next){
+
+      console.log("Trying to fetch Lessons for ", req.body.className);
+      var name = req.body.className
+      // name = name.replace(/\s/g, '')
+      var query = {'name': req.body.className};
+      Class.findOne(query, function(err, classes) {
+        if (err)
+          console.log(err);
+          // var classMap = {};
+          // var classMap = [];
+          //
+          // classes.forEach(function(c) {
+          //   classMap.push(c.name)
+          //
+          // });
+          if(!classes){
+            console.log('no class found')
+        }
+          if(classes){
+            console.log('classes: ', classes)
+
+            res.cookie('class' , req.body.className);
+            res.send(classes.lessons);
+        }
+      });
+      //save stock user selected as a cookie
+      // res.cookie('stock' , stock);
+      //send to next page
+      // res.send({url:'./getstock.html'})
+      });
+
+/*******************************************************
+** FETCH LESSON
+********************************************************/
+app.post('/fetchLesson', function(req,res,next){
+  var classN = req.body.classN;
+  var lesson = req.body.lesson;
+  console.log("Trying to fetch lesson: ", lesson, ' from ', classN);
+
+  var query = { 'name': lesson, 'className': classN};
+  Lesson.findOne(query, function(err, classes) {
+    if (err)
+      console.log(err);
+      // });
+      if(!classes){
+        console.log('no LESSON found')
+    }
+      if(classes){
+        res.cookie('lesson' , lesson);
+        res.cookie('url' , classes.url);
+        console.log('class id cookie is: ', String(classes._id))
+        res.cookie('lessonID', String(classes._id))
+        //send to next page
+
+        res.send({url:'./lesson.html'});
+    }
+
+  });
+});
+
+/*
+** On Socket Connection
+*/
+io.on('connection', function(socket){
+  console.log('new connection', socket.id);
+  // clients++;
+  // console.log('Clients now at', clients);
+
+  socket.on('join', function(data){
+    var count=0;
+    console.log(data.lessonID, "is the lesson ID socket is trying to join");
+    // socket.join(data.lessonID);
+
+    var query = { '_id': data.lessonID};
+    Lesson.findOne(query, function(err, classes) {
+      if (err)
+        console.log(err);
+        // });
+        if(!classes){
+          console.log('no LESSON found')
+      }
+        if(classes){
+          console.log(data.lessonID, "is the lesson ID socket joined");
+          console.log(classes.chat)
+          socket.join(data.lessonID);
+          socket.emit('getChat', {lessonID: data.lessonID, chat: classes.chat} ) ;
+          // res.cookie('lesson' , lesson);
+          // res.cookie('url' , classes.url);
+          // res.cookie('lessonID', classes._id)
+          //send to next page
+
+          // res.send({url:'./lesson.html'});
+      }
+
+    });
+
+
+  });
+
+  /*
+  ** On Chat Send Message
+  */
+  socket.on('sendMessage', function(data){
+      //Broadcast to all sockets in room
+    // wss.sockets.in(data.gameID).emit('messageSent', data);
+    var classN = data.classN;
+    var lesson = data.lessonID;
+    var user = data.userName;
+
+  var update = '<p><strong>' + data.userName +
+  ":  </strong>"+ data.message +'</p>'
+
+    var query = { '_id': data.lessonID};
+    Lesson.findOneAndUpdate(query, { $push: {chat: update} } ,
+           {upsert:true, new: true}, function(err, doc){
+          if (err)
+            console.log(err);
+          else{
+            console.log(" Chat DB Updated!");
+
+            // res.cookie('lessonID', doc._id)
+              // res.send({url:'./lesson.html'})
+              io.sockets.in(data.lessonID).emit('messageSent', data);
+          }
+        });
+
+
+
+    });
+
+
+
+
+
+
+
+
+  });
